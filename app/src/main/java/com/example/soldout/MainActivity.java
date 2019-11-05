c25f591ed39886b62a8bcf7fa269b974c012229e
porideki <porideki@gmail.com>
2019-11-05 22:27:28 +0900

    QRを読んでpop表示

--- app/src/main/java/com/example/soldout/MainActivity.java
+++ app/src/main/java/com/example/soldout/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.soldout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +19,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickedQRButton(View view){
+        Log.d("Debug", "Clicked!");
+
+        new IntentIntegrator(this).initiateScan();
+
+        //QRスキャナ起動
+        //Intent qrReaderIntent = new Intent(this, QRScanner.class);
+        //startActivity(qrReaderIntent);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Log.d("MainActivity", "Cancelled scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d("MainActivity", "Scanned");
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
 }

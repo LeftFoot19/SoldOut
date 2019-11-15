b461ab544a73f3f65af5ac1afad630915f47a8b6
porideki <porideki@gmail.com>
2019-11-16 02:42:02 +0900

    QRスキャナ起動時の設定を追加
    読み取り時Beep音を鳴らさない
    画面回転を固定しない(機能してない)
    QRコードのみ読み取り

--- app/src/main/java/com/example/soldout/manager/IntentManager.java
+++ app/src/main/java/com/example/soldout/manager/IntentManager.java
@@ -7,7 +7,11 @@ import com.google.zxing.integration.android.IntentIntegrator;
 public class IntentManager {
 
     public static void RunQRScanner(Activity returnTargetActivity){
-        new IntentIntegrator(returnTargetActivity).initiateScan();
+        new IntentIntegrator(returnTargetActivity)
+                .setBeepEnabled(false)                                  //読み取り時に音を鳴らさない
+                .setOrientationLocked(false)                            //画面の向きを固定しない(機能してない気がする)
+                .setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)  //QRコードのみ読み取り
+                .initiateScan();                                        //初期化・起動
     }
 
 }
